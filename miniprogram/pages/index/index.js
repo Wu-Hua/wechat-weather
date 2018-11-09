550ef0c1af6652379f5af8da342fe1d859a71eab
华 <569405489@qq.com>
2018-11-09 10:37:50 +0800

    添加天气api、动态更改温度、天气、天气背景、导航栏颜色

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,56 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const weatherBgMap = {
+  'sunny': 'sunny-bg.png',
+  'cloudy': 'cloudy-bg.png',
+  'overcast': 'overcast-bg.png',
+  'lightrain': 'lightrain-bg.png',
+  'heavyrain': 'heavyrain-bg.png',
+  'snow': 'snow-bg.png'
+}
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
 Page({
+  data: {
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherBg: ''
+  },
   onLoad() {
-    console.log('Hello World!');
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        console.log(temp,weather);
+        this.setData({
+          nowTemp: temp,
+          nowWeather: weatherMap[weather],
+          nowWeatherBg: '../../images/' + weatherBgMap[weather]
+        });
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      }
+    })
   }
 })
+
