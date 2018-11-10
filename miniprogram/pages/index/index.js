c12548a57128851c181a4cd62637f3b35e1b9a7e
华 <569405489@qq.com>
2018-11-10 16:19:35 +0800

    重构代码

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -21,10 +21,6 @@ const UNPROMPTED = 0
 const UNAUTHORIZED = 1
 const AUTHORIZED = 2
 
-const UNPROMPTED_TIPS = "点击获取当前位置"
-const UNAUTHORIZED_TIPS = "点击开启位置权限"
-const AUTHORIZED_TIPS = ""
-
 Page({
   data: {
     nowTemp: '',
@@ -34,7 +30,6 @@ Page({
     todayTemp: '',
     todayDate: '',
     city: '广州市',
-    locationTipsText: UNPROMPTED_TIPS,
     locationAuthType: UNPROMPTED 
   },
   onPullDownRefresh() {
@@ -52,11 +47,8 @@ Page({
         let auth = res.authSetting['scope.userLocation']
         let locationAuthType = auth ? AUTHORIZED
           : (auth === false) ? UNAUTHORIZED : UNPROMPTED
-        let locationTipsText = auth ? AUTHORIZED_TIPS
-          : (auth === false) ? UNAUTHORIZED_TIPS : UNPROMPTED_TIPS
         this.setData({
           locationAuthType: locationAuthType,
-          locationTipsText: locationTipsText
         })
 
         if (auth)
@@ -149,7 +141,6 @@ Page({
       success: res => {
         this.setData({
           locationAuthType: AUTHORIZED,
-          locationTipsText: AUTHORIZED_TIPS
         })
         this.qqmapsdk.reverseGeocoder({
           location: {
@@ -168,7 +159,6 @@ Page({
       fail: ()=> {
         this.setData({
           locationAuthType: UNAUTHORIZED,
-          locationTipsText: UNAUTHORIZED_TIPS
         })
       }
     })
